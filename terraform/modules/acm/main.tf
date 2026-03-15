bfb6d0262d2cd3cf62ef39571948bae4aa6150d4
Muhammad Rashel Mia <muhammadrashel2015@gmail.com>
2026-03-15 03:33:02 -0400

    enable HTTPS with ACM certificate

--- terraform/modules/acm/main.tf
+++ terraform/modules/acm/main.tf
@@ -3,16 +3,15 @@ data "aws_route53_zone" "main" {
   private_zone = false
 }
 
-# Request a TLS certificate from AWS Certificate Manager (free)
 resource "aws_acm_certificate" "main" {
-  domain_name       = var.domain_name
-  validation_method = "DNS"
+  domain_name               = var.root_domain
+  subject_alternative_names = ["*.${var.root_domain}"]
+  validation_method         = "DNS"
   lifecycle {
     create_before_destroy = true
   }
 }
 
-# Create DNS records in Route 53 to prove you own the domain
 resource "aws_route53_record" "validation" {
   for_each = {
     for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
@@ -29,7 +28,6 @@ resource "aws_route53_record" "validation" {
   zone_id         = data.aws_route53_zone.main.zone_id
 }
 
-# Wait for the certificate to be validated (takes ~2 minutes)
 resource "aws_acm_certificate_validation" "main" {
   certificate_arn         = aws_acm_certificate.main.arn
   validation_record_fqdns = [for r in aws_route53_record.validation : r.fqdn]
